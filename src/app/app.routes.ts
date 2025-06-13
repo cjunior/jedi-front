0f254f0b4a8ac1aa0e29a45e8a3d3ed50281ca44
Aglis Bernardino da Silva <aglis.silva62@aluno.ifce.edu.br>
2025-06-13 15:14:19 -0300

    refactor: removendo bootstrap e adicionando a lib primeng para biblioteca de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('../pages/lading-page/lading-page.component').then(m => m.LadingPageComponent)
+        loadComponent: () => import('./pages/lading-page/lading-page.component').then(m => m.LadingPageComponent)
     }
 ];
