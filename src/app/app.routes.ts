306ecac7ecd5450a41ee0be46553c09eaae046ce
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2025-06-13 23:01:31 +0100

    Feat/login (#3)
    * termiando a pagina de login
    
    * terminando a pagina de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./pages/lading-page/lading-page.component').then(m => m.LadingPageComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login/login.component').then(m => m.LoginComponent)
     }
 ];
