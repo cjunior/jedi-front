d5a9972fe3bc120038092f29bebf20c92e67415d
GeniltonSilva <geniltonsilva4002@gmail.com>
2025-06-14 19:44:07 -0300

    feat/ tentando editar as imagens

--- src/app/pages/user-configs/admin/manage-edit-text-landing-page/components/lading-page/components/carousel/carousel.component.ts
+++ src/app/pages/user-configs/admin/manage-edit-text-landing-page/components/lading-page/components/carousel/carousel.component.ts
@@ -3,15 +3,19 @@ import { Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
+import { ManageCarouselModalComponent } from '../../../modal/modal';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [CommonModule, CarouselModule, ButtonModule, TagModule],
+  imports: [CommonModule, CarouselModule, ButtonModule, TagModule, ManageCarouselModalComponent],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
+  showModal = false;
+  modalVisible = false;
+
   items = [
     { image: '/fotoend2.png', alt: 'Foto End 2' },
     { image: '/diva2.svg', alt: 'Diva 2' },
@@ -26,37 +30,23 @@ export class CarouselComponent {
   ];
 
   responsiveOptions = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5,
-      numScroll: 1
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3,
-      numScroll: 1
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1,
-      numScroll: 1
-    }
+    { breakpoint: '1024px', numVisible: 5, numScroll: 1 },
+    { breakpoint: '768px', numVisible: 3, numScroll: 1 },
+    { breakpoint: '560px', numVisible: 1, numScroll: 1 }
   ];
 
-  onFileSelected(event: Event, item: { image: string; alt: string }) {
-    const index = this.items.indexOf(item); // Calcula o índice do item
-    if (index !== -1) { // Verifica se o item existe no array
-      const input = event.target as HTMLInputElement;
-      if (input.files && input.files.length > 0) {
-        const file = input.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.items[index].image = reader.result as string;
-        };
-        reader.readAsDataURL(file);
-      }
-    } else {
-      console.error(`Índice inválido: ${index}`);
-    }
+  modalItems: any[] = [];
+
+  openModal() {
+    this.modalItems = [...this.items]; // Clona o array
+    this.modalVisible = true; // <- CORRETO
+  }
+  
+  onSave(newItems: any[]) {
+    this.items = [...newItems];
+  }
+  
+  updateItems(updatedItems: any[]) {
+    this.items = [...updatedItems];
   }
 }
