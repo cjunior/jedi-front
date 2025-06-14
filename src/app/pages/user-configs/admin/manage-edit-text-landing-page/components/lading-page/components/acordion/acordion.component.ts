ac34aafbec3d07d7938218746d9734b3779af3fb
GeniltonSilva <geniltonsilva4002@gmail.com>
2025-06-14 17:55:32 -0300

    feat/ adicionando acordion

--- src/app/pages/user-configs/admin/manage-edit-text-landing-page/components/lading-page/components/acordion/acordion.component.ts
+++ src/app/pages/user-configs/admin/manage-edit-text-landing-page/components/lading-page/components/acordion/acordion.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-acordion',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './acordion.component.html',
   styleUrls: ['./acordion.component.scss']
 })
@@ -12,7 +13,7 @@ export class AcordionComponent {
   items = [
     {
       title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat lobortis dui vitae laoreet. Suspendisse turpis ante, bibendum vel semper sit amet, vehicula in ligula. '
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat lobortis dui vitae laoreet. Suspendisse turpis ante, bibendum vel semper sit amet, vehicula in ligula.'
     },
     {
       title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
@@ -29,18 +30,30 @@ export class AcordionComponent {
   ];
 
   activeIndex: number | null = null;
-  heights: number[] = [];
+  editingIndex: number | null = null;
 
   @ViewChildren('content') contentElements!: QueryList<ElementRef>;
 
   ngAfterViewInit() {
     // Calcula as alturas de cada conteúdo após a renderização
-    this.heights = this.contentElements.map(
-      (content) => content.nativeElement.scrollHeight
-    );
+    this.contentElements.forEach((content) => {
+      content.nativeElement.scrollHeight;
+    });
   }
 
   toggle(index: number) {
     this.activeIndex = this.activeIndex === index ? null : index;
+  }
+
+  startEditing(index: number) {
+    this.editingIndex = index;
+  }
+
+  saveEditing() {
+    this.editingIndex = null;
+  }
+
+  cancelEditing() {
+    this.editingIndex = null;
   }
 }
