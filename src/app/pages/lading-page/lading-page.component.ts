aa14d31f49a0b423ee3a8e2f9053fc49d65d1239
GeniltonSilva <geniltonsilva@gmail.com>
2025-06-18 16:33:25 -0300

    integrando a landing page

--- src/app/pages/lading-page/lading-page.component.ts
+++ src/app/pages/lading-page/lading-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CarouselComponent } from "./components/carousel/carousel.component";
 
 import { AcordionComponent } from "./components/acordion/acordion.component";
@@ -13,6 +13,7 @@ import { InputMaskModule } from 'primeng/inputmask';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Message } from 'primeng/message';
 import { landingPageService } from './services/lading-page.service';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-lading-page',
@@ -29,12 +30,13 @@ import { landingPageService } from './services/lading-page.service';
     InputMaskModule,
     Message,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CarouselModule
   ],
   templateUrl: './lading-page.component.html',
   styleUrl: './lading-page.component.scss'
 })
-export class LadingPageComponent {
+export class LadingPageComponent implements OnInit {
   private readonly formBuilder = inject(FormBuilder)
   private readonly landingPageService = inject(landingPageService);
   menuAberto = false;
@@ -45,15 +47,76 @@ export class LadingPageComponent {
     email: ['', [Validators.email, Validators.required]],
     phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]],
   });
+  ngOnInit() {
+  this.landingPageService.getdados().subscribe({
+    next: (dados) => {
+      console.log('Dados recebidos:', dados);
+      this.headerResponseDto = {
+        urllogo: dados.headerResponseDto.logoUrl,
+        projeto: dados.headerResponseDto.text1,
+        conteudo: dados.headerResponseDto.text2,
+        ajuda: dados.headerResponseDto.text3,
+        red: dados.headerResponseDto.text4,
+        buttontext: dados.headerResponseDto.buttonText
+      }
+      this.bannerResponseDto = {
+        title: dados.bannerResponseDto.title,
+        description: dados.bannerResponseDto.description
+      }
+      this.presentationSectionResponseDto = {
+        title: dados.presentationSectionResponseDto.title,
+        firstDescription: dados.presentationSectionResponseDto.firstDescription,
+        secondDescription: dados.presentationSectionResponseDto.secondDescription,
+        firstStatistic: dados.presentationSectionResponseDto.firstStatistic,
+        secondStatistic:dados.presentationSectionResponseDto.secondStatistic,
+        imgUrl: dados.presentationSectionResponseDto.imgUrl,
+        imgDescription: dados.presentationSectionResponseDto.imgDescription
+      }
+      this.faqSectionResponseDto = {
+        title: dados.faqSectionResponseDto.title,
+        subtitle: dados.faqSectionResponseDto.subtitle
+      }
+      this.carouselImages = dados.bannerResponseDto.items.map((item: any) => item.imgUrl);
+    }
+  })
+}
 
-  menu = 
+headerResponseDto = 
     {
+      urllogo: './logo.svg',
       projeto: 'Projeto',
       conteudo: 'Conteúdo',
       ajuda: 'Ajuda',
       red: '#RedeJED',
       buttontext: 'Entrar'
     }
+
+    bannerResponseDto = {
+      title: "DÊ UM PLAY NO SEU FUTURO",
+      description: "Curso online com formação personalizada para você empreender de forma inteligente e estratégica"
+      
+    }
+
+    presentationSectionResponseDto = {
+      title: "O projeto",
+      firstDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac ullamcorper metus. Nunc cursus orci tortor, sed interdum mi commodo a.",
+      secondDescription: "Duis fermentum velit at sapien iaculis tincidunt. Integer ultrices mollis sagittis. Nulla facilisi. Nulla facilisi.",
+      firstStatistic: "# de estudantes",
+      secondStatistic: "# de alcance",
+      imgUrl: "./divos.svg",
+      imgDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac ullamcorper metus."
+    }
+
+    faqSectionResponseDto = {
+      title: "Perguntas frequentes",
+      subtitle: "Dúvidas comuns sobre o curso",
+    }
+
+    carouselImages = [
+      './fotoH.jpg',
+      './fotoH2.jpg',
+      './fotoH3.jpg'
+    ];
   
 
   cards = [
@@ -79,16 +142,6 @@ export class LadingPageComponent {
       tempoLeitura: '2min de leitura'
     }
   ];
-  ngOnInit(): void {
-    this.landingPageService.getdados().subscribe({
-      next: (dados) => {
-       
-      },
-      error: (err) => {
-        console.error('Erro ao buscar dados:', err);
-      }
-    });
-  }
 
   toggleMenu() {
     this.form.reset()
--- src/app/pages/lading-page/services/lading-page.service.ts
+++ src/app/pages/lading-page/services/lading-page.service.ts
@@ -1,17 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class landingPageService {
     private readonly http = inject(HttpClient);
-    private readonly apiUrl = 'http://localhost:3000';
+    private readonly apiUrl = environment.apiUrl;
 
 
     getdados(): Observable<any> {
-        return this.http.get(`${this.apiUrl}/clientes`);
+        return this.http.get(`${this.apiUrl}loadlandpage/get`);
       }
  
 }
