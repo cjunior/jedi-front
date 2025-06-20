2b4a285f0280fbd00d4dc60959c8b0f837ba66e9
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2025-06-20 02:06:30 -0300

    atualizando (#28)

--- src/app/pages/lading-page/components/carousel-content/carousel.component.ts
+++ src/app/pages/lading-page/components/carousel-content/carousel.component.ts
@@ -12,34 +12,21 @@ import { landingPageService } from '../../services/lading-page.service';
 })
 export class CarouselContentComponent implements OnInit {
   private readonly landingPageService = inject(landingPageService);
-  items = [
-    { image: '/diva2.svg', alt: 'Diva 2' },
-    { image: '/fotoca.png', alt: 'Fotoca' },
-    { image: '/diva2.svg', alt: 'Diva 2' },
-    { image: '/fotoca.png', alt: 'Fotoca' },
-    { image: '/diva2.svg', alt: 'Diva 2' },
-    { image: '/fotoca.png', alt: 'Fotoca' },
-    { image: '/diva2.svg', alt: 'Diva 2' },
-    { image: '/fotoca.png', alt: 'Fotoca' },
-    { image: '/diva2.svg', alt: 'Diva 2' },
-    { image: '/fotoca.png', alt: 'Fotoca' }
-  ];
+items: { image: string; alt: string; text: string }[] = [];
 
-  ngOnInit(): void {
-    this.landingPageService.getdados().subscribe({
-      next: (response) => {
-          
-        this.items = response.redeJediSectionDto.imagens.map((slide: any) => ({
-          image: slide.url,
-        
-        
-        }));
-      },
-      error: (error) => {
-        console.error('Error fetching carousel data:', error);
-      }
-    })
-  }
+ngOnInit() {
+  this.landingPageService.getdados().subscribe({
+    next: (dados) => {
+      // Se quiser pegar as imagens do redeJediSectionDto:
+      this.items = (dados.redeJediSectionDto.imagens || []).map((img: any) => ({
+        image: img.url,
+        alt: img.publicId || 'Imagem do item',
+        text: img.publicId || ''
+      }));
+    }
+  });
+}
+
 
   responsiveOptions = [
     {
