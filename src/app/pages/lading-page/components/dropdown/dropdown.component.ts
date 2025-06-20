5f97e02dddcc201d5b2e9925910f831a93435649
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2025-06-20 13:25:55 -0300

    Development (#38)
    * Feat/integracao manage pre register (#18)
    
    * feat: adicionando paginação na tabela da tela de gerenciamento dos pré-registros
    
    * refactor: voltando link da api
    
    * Feat/completando integração (#19)
    
    * integrando a landing page
    
    * feat/ integrando o 5 container
    
    * feat/ integrando carrossel de equipe
    
    * feat/ começando a integração do acordion
    
    * feat/ integração da parte de gerencia
    
    * feat/ integrando landingpage
    
    * feat/ adicionando estilizaçao
    
    * concluindo integração da landing page
    
    ---------
    
    Co-authored-by: aglis silva <111001349+aaglis@users.noreply.github.com>
    
    * feat/ adicionando o responsetext
    
    * limpnado as requisições
    
    * feat/ alteração la landing page
    
    * fazendo a integração dos filtros da tela de gerenciamento de pré registros (#23)
    
    * Feat/integração blog (#25)
    
    * feat; terminando a integração da landing page
    
    * feat/ adicionar
    
    * atualizando (#28)
    
    * Refactor/ajustes validacao (#30)
    
    * refactor: ajustando validação no modal de pré registro e completar cadastro
    
    * fix: comentando duplicação de oninit no componente de carrossel
    
    * feat/arrumando as imagens
    
    * git
    
    * arrumando tudo
    
    * feat: subindo as estilizacoes
    
    * refacotor: ajustando responsividade da sidebar da tela de gerenciamento (#34)
    
    * feat/ arrumando requisição
    
    * feat/ aumentando os caractere
    
    * feat/ mobile
    
    * feat/ adionando toast
    
    * feat/ bug das imagens
    
    ---------
    
    Co-authored-by: aglis silva <111001349+aaglis@users.noreply.github.com>
    Co-authored-by: Aglis Bernardino da Silva <aglis.silva62@aluno.ifce.edu.br>

--- src/app/pages/lading-page/components/dropdown/dropdown.component.ts
+++ src/app/pages/lading-page/components/dropdown/dropdown.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { landingPageService } from '../../services/lading-page.service';
 
 @Component({
   selector: 'app-dropdown',
@@ -8,11 +9,25 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss']
 })
-export class DropdownComponent {
+export class DropdownComponent implements OnInit {
   isMenuOpen = false;
+  private readonly landingpageurl = inject(landingPageService);
 
+ header: any = {};
+ 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngOnInit(): void {
+    this.landingpageurl.getdados().subscribe({
+      next: (data) => {
+        this.header = data.headerResponseDto || {};
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+    });
   }
 
   @HostListener('document:click', ['$event'])
