844e85de947705a9bdef1d97aa66a1fac8418cbb
aaglis <aglissilvafon@gmail.com>
2025-06-24 16:04:48 -0300

    fix: ajustando validação do rg e liberando arquivos como pdf para validação de rg e comprovante de residência

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,14 @@ import Aura from '@primeng/themes/aura';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import ptBr from "@angular/common/locales/pt"
+import { provideNgxMask } from "ngx-mask"
 
 registerLocaleData(ptBr)
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideNgxMask(),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     importProvidersFrom(HttpClientModule),
     provideAnimations(),
