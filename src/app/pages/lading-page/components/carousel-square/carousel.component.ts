dd218c323ef6abdd676fb8448782af5d1dd765b2
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2025-06-25 21:16:38 -0300

    Development (#58)
    * feat/ populabdo a tabela
    
    * feat/ tirando card

--- src/app/pages/lading-page/components/carousel-square/carousel.component.ts
+++ src/app/pages/lading-page/components/carousel-square/carousel.component.ts
@@ -8,15 +8,64 @@ import { landingPageService } from '../../services/lading-page.service';
   standalone: true,
   imports: [CommonModule, CarouselModule],
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselSquareComponent implements OnInit {
   private readonly landingPageService = inject(landingPageService);
 
   items = [
-    { image: './conteudo.png', alt: 'Banner 1', text: 'Seu Produto na Internet' },
-    { image: './conteudo.png', alt: 'Banner 1', text: 'Venda Mais na Internet' },
-    { image: './conteudo.png', alt: 'Banner 1', text: 'Apoio Digital ao Negócio' },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Seu Produto na Internet',
+    },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Venda Mais na Internet',
+    },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Apoio Digital ao Negócio',
+    },
+   
+
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Empreendedorismo' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Business CANVAS ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Personograma ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Marketing ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Vendas dropshipping ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Vendas PLR ' },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Afiliados infoprodutores ',
+    },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Afiliados encapsulados ',
+    },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Digital influencer ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Vendas no Instagram ' },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Cooperativismo de Plataforma ',
+    },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Copywriting ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Ferramentas de IA ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Vendas no Facebook ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'UI/UX ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Vendas no WPP ' },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Social mídia ' },
+    {
+      image: './conteudo.png',
+      alt: 'Banner 1',
+      text: 'Vendas utilizando o TikTok ',
+    },
+    { image: './conteudo.png', alt: 'Banner 1', text: 'Técnicas de design ' },
 
   ];
 
@@ -35,17 +84,17 @@ export class CarouselSquareComponent implements OnInit {
     {
       breakpoint: '1024px',
       numVisible: 3,
-      numScroll: 1
+      numScroll: 1,
     },
     {
       breakpoint: '768px',
       numVisible: 2,
-      numScroll: 1
+      numScroll: 1,
     },
     {
       breakpoint: '560px',
       numVisible: 1,
-      numScroll: 1
-    }
+      numScroll: 1,
+    },
   ];
 }
