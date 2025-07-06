ead618366813c43e07ea90199ab9d8614881cfdf
Genilton Silva <geniltonsilva4002@gmail.com>
2025-07-06 17:42:43 -0300

    criando carrosseol novo

--- src/app/pages/lading-page/components/carousel-square/carousel.component.ts
+++ src/app/pages/lading-page/components/carousel-square/carousel.component.ts
@@ -1,157 +1,41 @@
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
-import { landingPageService } from '../../services/lading-page.service';
 
 @Component({
   selector: 'app-carousel-square',
   standalone: true,
-  imports: [CommonModule, CarouselModule],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselSquareComponent implements OnInit {
-  private readonly landingPageService = inject(landingPageService);
+export class CarouselSquareComponent {
+  currentIndex = 0;
 
-  items = [
+  slides = [
     {
-      image: './Afiliadosinfoprodutores.jpg',
-      alt: 'Banner',
-      text: 'Afiliados infoprodutores',
-    },
-    {
-      image: './Afiliadosencapsulados.jpg',
-      alt: 'Banner',
-      text: 'Afiliados encapsulados',
-    },
-    { 
-      image: './BusinessCANVAS.png', 
-      alt: 'Banner', 
-      text: 'Business CANVAS' 
+      type: 'gradient',
+      gradient: 'bg-gradient-to-r from-white to-lime-500',
+      text: 'KIT DE APRENDIZAGEM',
     },
     {
-      image: './cooperativismo.jpeg',
-      alt: 'Banner',
-      text: 'Cooperativismo de plataforma',
-    },
-    { 
-      image: './Copywriting.jpg', 
-      alt: 'Banner', 
-      text: 'Copywriting' 
-    },
-    { 
-      image: './Digitalinfluencer.jpg', 
-      alt: 'Banner', 
-      text: 'Digital influencer' 
-    },
-    { 
-      image: './tecmemp.jpeg', 
-      alt: 'Banner', 
-      text: 'Técnicas de empreendedorismo' 
-    },
-    { 
-      image: './FerramentasdeIA.jpg', 
-      alt: 'Banner', 
-      text: 'Ferramentas de IA' 
-    },
-    { 
-      image: './Marketing.jpg', 
-      alt: 'Banner', 
-      text: 'Marketing' 
-    },
-    { 
-      image: './Personograma.png', 
-      alt: 'Banner', 
-      text: 'Personograma' 
-    },
-    { 
-      image: './Socialmedia.jpg', 
-      alt: 'Banner', 
-      text: 'Social media' 
-    },
-    { 
-      image: './tecnicas.jpg', 
-      alt: 'Banner', 
-      text: 'Técnicas de design' 
-    },
-    { 
-      image: './TráfegoPago.png', 
-      alt: 'Banner', 
-      text: 'Tráfego pago' 
-    },
-    { 
-      image: './ux.jpg', 
-      alt: 'Banner', 
-      text: 'UI/UX' 
-    },
-    { 
-      image: './businessman-planning-strategy.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas dropshipping' 
-    },
-    { 
-      image: './vendas.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas de e-commerce' 
-    },
-    { 
-      image: './facebook.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas no Facebook' 
-    },
-    { 
-      image: './ifood.jpeg', 
-      alt: 'Banner', 
-      text: 'Vendas no iFood' 
-    },
-    { 
-      image: './instagram.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas no Instagram' 
-    },
-    { 
-      image: './mercadolivre.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas no Mercado Livre' 
+      type: 'image',
+      imageUrl: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2',
+      text: 'KIT DE APRENDIZAGEM',
     },
     {
-      image: './tiktok.jpg',
-      alt: 'Banner',
-      text: 'Vendas no TikTok',
-    },
-    { 
-      image: './whatsapp.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas no WhatsApp' 
-    },
-    { 
-      image: './youtube.jpg', 
-      alt: 'Banner', 
-      text: 'Vendas no YouTube' 
-    },
-    { 
-      image: './VendasPLR.jpeg', 
-      alt: 'Banner', 
-      text: 'Vendas PLR' 
+      type: 'gradient',
+      gradient: 'bg-gradient-to-r from-orange-200 to-orange-500',
+      text: 'KIT DE APRENDIZAGEM',
     },
   ];
 
-  isMobile = false;
-
-  ngOnInit() {
-    this.checkMobile();
-    window.addEventListener('resize', () => this.checkMobile());
+  
+  previous() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.slides.length) % this.slides.length;
   }
 
-  checkMobile() {
-    this.isMobile = window.innerWidth <= 770;
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
   }
-
-  responsiveOptions = [
-    {
-      breakpoint: '770px',
-      numVisible: 1,
-      numScroll: 1,
-    },
-  ];
 }
