0b1e8fffd694b1c038a6a5b0eb0fa6e579c19eb7
Genilton Silva <geniltonsilva4002@gmail.com>
2025-07-09 17:30:24 -0300

    feat/ refinando o codigo

--- src/app/pages/lading-page/components/acordion/acordion.component.ts
+++ src/app/pages/lading-page/components/acordion/acordion.component.ts
@@ -30,14 +30,14 @@ export class AcordionComponent implements OnInit {
     },
     {
       title: 'O curso é presencial ou on-line?',
-      content: 'As aulas são online, com alguns encontros presenciais no município onde você se inscrever.'
+      content: 'As aulas são on-line, com alguns encontros presenciais no município onde você se inscrever.'
     },
     {
       title: 'Em quais municípios do Pará o curso será realizado? ',
       content: 'O curso está disponível nos seguintes municípios: Abaetetuba, Acará, Alenquer, Altamira, Ananindeua, Baião, Barcarena, Belém, Benevides, Bragança, Breu Branco, Breves, Cametá, Canaã dos Carajás, Capanema, Capitão-Poço, Castanhal, Conceição do Araguaia, Curuçá, Dom Eliseu, Igarapé-Miri, Itaituba, Itupiranga, Jacundá, Juruti, Marabá, Marituba, Mojú, Monte Alegre, Novo Repartimento, Óbidos, Oriximiná, Pacajá, Paragominas, Parauapebas, Portel, Redenção, Rondon do Pará, Salinópolis, Santa Izabel do Pará, Santarém, São Felix do Xingu, São Miguel do Guamá, Tailândia, Terra Santa, Tomé-Açú, Tucumã, Tucuruí, Ulianópolis, Uruará, Vigia, Viseu e Xinguara.'
     },
     {
-      title: 'Posso participar do curso mesmo que não more em uma cidade polo? ',
+      title: 'Posso participar do curso mesmo não morando em uma cidade polo?',
       content: 'Não. É necessário comprovar residência no município onde o curso será realizado.'
     }
   ];
